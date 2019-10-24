88b0d1cf22f2407346d953ca0fe6bf07e6c75825
壮 <wang895736509@126.com>
2019-10-24 20:47:46 +0800

    版本4

--- hello.py
+++ hello.py
@@ -1,7 +1,18 @@
-print("helloworld")
-list = [1,2,3,4,5]
-it = iter(list)
-for it in list:
-    print(it, end='')
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
 
+    def __next__(self):
+        if self.a <= 20:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            raise StopIteration
 
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for i in myiter:
+    print(i)
